58c3b1eb3c0f0e0f77b0c92063d93466c51e2c0d
Will <will.liu@nakedhub.cn>
2017-08-15 18:01:14 +0800

    1.1.0

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ mongoose.Promise = global.Promise;
  * @param {object} schema 数据表结构
  * @param {object} conn   已打开数据库连接
  */
-function Model(name, schema, conn){
+function ModelClass(name, schema, conn) {
 	this.conn = conn;
-	this.model = this.conn.model(name, new mongoose.Schema(schema));
+	this.model = this.conn.model(name, new mongoose.Schema(schema), name);
 }
 
-Model.prototype = {
+ModelClass.prototype = {
+	query: function(query) {
+		query = query || {};
+
+		return this.model.find(query);
+	},
+
 	/**
 	 * 查询数据
 	 * @param  {object}   query    查询条件，参考mongoose相关说明
@@ -23,35 +29,37 @@ Model.prototype = {
 	 * @param  {Function} callback 查询成功后的回调函数，第一个参数是error，第二个是查询结果
 	 * @return {Query}             mongoose返回的对象
 	 */
-	get: function(query, options, callback){
-		let Model = this.model;
+	get: function(query, options, callback) {
+		const Model = this.model;
 		let Query = null;
 
-		if( typeof(query) == 'function' ){
+		if (typeof (query) == 'function') {
 			callback = query;
 			query = {};
 		} else {
 			query = query || {};
 			options = options || {};
-			callback = callback || function(){};
-			if( typeof(options) == 'function' ){
+			callback = callback || function() {};
+			if (typeof (options) == 'function') {
 				callback = options;
 				options = {};
 			}
 
-			if( options.pageSize ){
-				if( options.lastId ){
-					query['_id'] = {'$gt': ObjectId(options.lastId)};
+			if (options.pageSize) {
+				if (options.lastId) {
+					query['_id'] = {$gt: mongoose.Schema.Types.ObjectId(options.lastId)};
 					Query = Model.find(query).limit(options.pageSize);
 				} else {
 					options.page = options.page || 1;
-					Query = Model.find(query).skip(options.pageSize * (options.page - 1)).limit(options.pageSize);
+					Query = Model.find(query)
+								.skip(options.pageSize * (options.page - 1))
+								.limit(options.pageSize);
 				}
 				options.sort = options.sort || -1;
 			}
-			if( options.sort ){
+			if (options.sort) {
 				Query = Query || Model.find(query);
-				Query.sort({'_id': options.sort});
+				Query.sort({_id: options.sort});
 			}
 		}
 
@@ -60,28 +68,53 @@ Model.prototype = {
 	},
 
 	/**
-	 * 按表字段_id查询数据
-	 * @param  {string}   id       查询的_id值
-	 * @param  {Function} callback 查询成功后的回调函数，第一个参数是error，第二个是查询结果
-	 * @return {Query}             mongoose返回的对象
-	 */
-	getById: function(id, callback){
-		return this.get({_id: id}, callback);
-	},
-
-	/**
 	 * 无条件查询
 	 * @param  {object}   options  查询附加选项，如page，sort等
 	 * @param  {Function} callback 查询成功后的回调函数，第一个参数是error，第二个是查询结果
 	 * @return {Query}             mongoose返回的对象
 	 */
-	getAll: function(options, callback){
-		callback = callback || function(){};
-		if( typeof(options) == 'function' ){
+	getAll: function(options, callback) {
+		callback = callback || function() {};
+		if (typeof (options) == 'function') {
 			callback = options;
 			options = {};
 		}
+
 		return this.get({}, options, callback);
+	},
+
+	/**
+	 * 按表字段_id查询数据
+	 * @param  {string}   id       查询的_id值
+	 * @param  {Function} callback 查询成功后的回调函数，第一个参数是error，第二个是查询结果
+	 * @return {Query}             mongoose返回的对象
+	 */
+	getById: function(id, callback) {
+		const Model = this.model;
+
+		callback = callback || function() {};
+
+		return Model.findById(id, function(error, result) {
+			if (error) {
+				callback(error, null);
+				return;
+			}
+			callback(null, result);
+		});
+	},
+
+	getOne: function(query, callback) {
+		const Model = this.model;
+
+		callback = callback || function() {};
+
+		return Model.findOne(query, function(error, result) {
+			if (error) {
+				callback(error, null);
+				return;
+			}
+			callback(null, result);
+		});
 	},
 
 	/**
@@ -90,23 +123,27 @@ Model.prototype = {
 	 * @param {object}   data     新的数据
 	 * @param {Function} callback
 	 */
-	set: function(query, data, callback){
-		let Model = this.model;
+	set: function(query, data, callback) {
+		const Model = this.model;
 
-		callback = callback || function(){};
+		callback = callback || function() {};
 
 		return Model.find(query, (error, result) => {
-			if (error) return callback(error, null);
-
+			if (error) {
+				callback(error, null);
+				return;
+			}
 			if (result && result.length > 0) {
 				Model.update(query, {$set: data}, (updateErr) => {
-					if (updateErr) return callback(updateErr, null);
-					
+					if (updateErr) {
+						callback(updateErr, null);
+						return;
+					}
 					this.get({_id: result[0]._id}, callback);
 				});
-			} else {
-				callback({message: 'not found'}, null);
+				return;
 			}
+			callback({message: 'not found'}, null);
 		});
 	},
 
@@ -116,15 +153,17 @@ Model.prototype = {
 	 * @param {object}   data     新的数据
 	 * @param {Function} callback
 	 */
-	setById: function(id, data, callback){
-		let Model = this.model;
+	setById: function(id, data, callback) {
+		const Model = this.model;
 
-		callback = callback || function(){};
+		callback = callback || function() {};
 
 		return Model.findByIdAndUpdate(id, {$set: data}, (error, result) => {
-			if (error) return callback(error, null);
-			
-			this.get({_id: id}, callback);
+			if (error) {
+				callback(error, null);
+				return;
+			}
+			this.getById(id, callback);
 		});
 	},
 
@@ -132,31 +171,29 @@ Model.prototype = {
 	 * 更新数据，参照mongoose的方式更新
 	 * @param {object}   query    查询条件
 	 * @param {object}   update   更新数据的对象，参考mongoose相关说明
-	 * @param {object}   options  更新数据的额外属性，参考mongoose相关说明
 	 * @param {Function} callback
 	 */
-	update: function(query, update, options, callback){
-		let Model = this.model;
+	update: function(query, update, callback) {
+		const Model = this.model;
 
-		options = options || {};
-		callback = callback || function(){};
-		if( typeof(options) == 'function' ){
-			callback = options;
-			options = {};
-		}
+		callback = callback || function() {};
 
 		return Model.find(query, (error, result) => {
-			if (error) return callback(error, null);
-
-			if (result && result.length > 0) {
-				Model.update(query, update, options, (updateErr) => {
-					if (updateErr) return callback(updateErr, null);
-					
-					this.get({_id: result[0]._id}, callback);
-				});
-			} else {
-				callback({message: 'not found'}, null);
+			if (error) {
+				callback(error, null);
+				return;
 			}
+			if (result && result.length > 0) {
+				Model.update(query, update, (updateErr) => {
+					if (updateErr) {
+						callback(updateErr, null);
+						return;
+					}
+					this.get(query, callback);
+				});
+				return;
+			}
+			callback({message: 'not found'}, null);
 		});
 	},
 
@@ -164,23 +201,19 @@ Model.prototype = {
 	 * 通过_id更新数据，参照mongoose的方式更新
 	 * @param {string}   id       查询的_id值
 	 * @param {object}   update   更新数据的对象，参考mongoose相关说明
-	 * @param {object}   options  更新数据的额外属性，参考mongoose相关说明
 	 * @param {Function} callback
 	 */
-	updateById: function(id, update, options, callback){
-		let Model = this.model;
+	updateById: function(id, update, callback) {
+		const Model = this.model;
 
-		options = options || {};
-		callback = callback || function(){};
-		if( typeof(options) == 'function' ){
-			callback = options;
-			options = {};
-		}
+		callback = callback || function() {};
 
-		return Model.findByIdAndUpdate(id, update, options, (error, result) => {
-			if (error) return callback(error, null);
-			
-			this.get({_id: id}, callback);
+		return Model.findByIdAndUpdate(id, update, (error, result) => {
+			if (error) {
+				callback(error, null);
+				return;
+			}
+			this.getById(id, callback);
 		});
 	},
 
@@ -189,22 +222,27 @@ Model.prototype = {
 	 * @param {object}   query    查询条件
 	 * @param {Function} callback
 	 */
-	delete: function(query, callback){
-		let Model = this.model;
+	delete: function(query, callback) {
+		const Model = this.model;
 
-		callback = callback || function(){};
+		callback = callback || function() {};
 
 		return Model.find(query, (error, result) => {
-			if (error) return callback(error, null);
-
+			if (error) {
+				callback(error, null);
+				return;
+			}
 			if (result && result.length > 0) {
 				Model.remove(query, (deleteErr) => {
-					if (deleteErr) return callback(deleteErr, null);
+					if (deleteErr) {
+						callback(deleteErr, null);
+						return;
+					}
 					callback(null, result);
 				});
-			} else {
-				return callback({message: 'not found'}, null);
+				return;
 			}
+			callback({message: 'not found'}, null);
 		});
 	},
 
@@ -213,13 +251,16 @@ Model.prototype = {
 	 * @param {string}   id       查询的_id值
 	 * @param {Function} callback
 	 */
-	deleteById: function(id, callback){
-		let Model = this.model;
+	deleteById: function(id, callback) {
+		const Model = this.model;
 
-		callback = callback || function(){};
+		callback = callback || function() {};
 
 		return Model.findByIdAndRemove(id, (error, result) => {
-			if (error) callback(error, null);
+			if (error) {
+				callback(error, null);
+				return;
+			}
 			callback(null, result);
 		});
 	},
@@ -230,13 +271,16 @@ Model.prototype = {
 	 * @param  {Function} callback 回调函数，第二个参数为新建的记录
 	 * @return {Promise}           一个promise对象
 	 */
-	create: function(data, callback){
-		let Model = this.model;
+	create: function(data, callback) {
+		const Model = this.model;
 
-		callback = callback || function(){};
+		callback = callback || function() {};
 
 		return Model.create(data, (error, result) => {
-			if (error) return callback(error, null);
+			if (error) {
+				callback(error, null);
+				return;
+			}
 			callback(null, result);
 		});
 	}
@@ -248,25 +292,65 @@ Model.prototype = {
  * @param {string} name 数据库操作名
  * @param {...}    args 打开数据库连接的参数
  */
-function Connection(name, ...args){
+function Connection(name, ...args) {
 	this.name = name;
-	this.conn = mongoose.createConnection();
 	this.connected = false;
 
-	if( args.length > 0 ){
+	this.conn = mongoose.createConnection();
+
+	if (args.length > 0) {
 		this.open(...args);
 	}
 }
 
 Connection.prototype = {
 	/**
+	 * 格式化打开数据库连接的参数
+	 * @param  {...} args 打开数据库连接的参数
+	 */
+	formatUri: function(...args) {
+		const dbServer = args[0], dbName = args[1];
+		let dbOptions, callback;
+
+		dbOptions = args[2];
+		callback = args[3];
+
+		if (typeof (dbOptions) == 'function') {
+			callback = dbOptions;
+			dbOptions = {};
+		} else {
+			if (Object.prototype.toString.call(dbOptions) !== '[object Object]') {
+				dbOptions = {};
+			}
+			if (typeof (callback) != 'function') {
+				callback = function() {};
+			}
+		}
+
+		if (dbOptions.user && dbOptions.pass) {
+			this.connectUri = 'mongodb://' + dbOptions.user + ':' + dbOptions.pass + '@' + dbServer + ':' + (dbOptions.port || 27017);
+		} else {
+			this.connectUri = 'mongodb://' + dbServer + ':' + (dbOptions.port || 27017);
+		}
+		delete dbOptions.user;
+		delete dbOptions.pass;
+		delete dbOptions.port;
+		dbOptions.useMongoClient = true;
+
+		this.connectUri += '/' + dbName;
+		this.connectOptions = dbOptions;
+
+		return callback;		
+	},
+
+	/**
 	 * 打开数据库连接
 	 * @param  {...} args 打开数据库连接的参数
 	 */
-	open: function(...args){
-		this.conn.open(...args);
+	open: function(...args) {
+		const callback = this.formatUri(...args);
+		this.conn.openUri(this.connectUri, this.connectOptions, callback);
 		this.conn.once('connected', () => {
-			this.connectUri = args[0];
 			this.connected = true;
 		});
 	},
@@ -275,7 +359,7 @@ Connection.prototype = {
 	 * 关闭数据库连接
 	 * @param  {Function} callback 关闭后的回调函数
 	 */
-	close: function(callback){
+	close: function(callback) {
 		this.conn.close(() => {
 			this.connected = false;
 			callback && callback();
@@ -286,9 +370,9 @@ Connection.prototype = {
 	 * 定义数据库可操作的表结构（打开数据表）
 	 * @param {object} models 表结构的定义集合
 	 */
-	setModels: function(models){
-		for( let name in models ){
-			this[name] = new Model(name, models[name], this.conn);
+	setModels: function(models) {
+		for (const name in models) {
+			this[name] = new ModelClass(name, models[name], this.conn);
 		}
 	}
 };
@@ -297,11 +381,11 @@ Connection.prototype = {
 module.exports = {
 	_connections: {},
 
-	createConnection: function(name, ...args){
-		if( this._connections[name] ){
-			let connection = this._connections[name];
-			if( connection.connected == true ){
-				connection.close(function(){
+	connectDatebase: function(connectedName, ...args) {
+		if (this._connections[connectedName]) {
+			const connection = this._connections[connectedName];
+			if (connection.connected == true) {
+				connection.close(function() {
 					connection.connected = false;
 					connection.open(...args);
 				});
@@ -309,18 +393,21 @@ module.exports = {
 				connection.open(...args);
 			}
 		} else {
-			this._connections[name] = new Connection(name, ...args);
-			this[name] = this._connections[name];
+			this[connectedName] = new Connection(connectedName, ...args);
+			this._connections[connectedName] = this[connectedName];
 		}
+
+		return this[connectedName];
 	},
 
-	closeConnection: function(name, callback){
-		if( name && this._connections[name] ){
-			let connection = this._connections[name];
-			connection.close(function(){
-				connection.connected = false;
-				callback && callback();
-			});
+	closeDatabase: function(connectedName, callback) {
+		if (!this._connections[connectedName]) {
+			return;
 		}
+		const connection = this._connections[connectedName];
+		connection.close(function() {
+			connection.connected = false;
+			callback && callback();
+		});
 	}
 };
